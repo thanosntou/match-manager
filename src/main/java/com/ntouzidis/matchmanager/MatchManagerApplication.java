2681e76d81016cc3ba843b0f9f2564bcae73882e
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2022-06-07 00:33:24 +0300

    Init project

---
+++ src/main/java/com/ntouzidis/matchmanager/MatchManagerApplication.java
@@ -0,0 +1,12 @@
+package com.ntouzidis.matchmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MatchManagerApplication.class, args);
+  }
+}
