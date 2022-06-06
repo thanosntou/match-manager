6efceea28ea2204162a6ad101d388ee90b940d30
Thanos Ntouzidis <thanosntouzidis@gmail.com>
2022-06-07 00:34:23 +0300

    Flyway/db configuration

---
+++ src/test/java/com/ntouzidis/matchmanager/MatchManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntouzidis.matchmanager;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchManagerApplicationTests {
+
+	//TODO fix this
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
